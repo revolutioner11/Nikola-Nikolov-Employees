c2958ea22faa699d67b4e200d5a5e2372a2392af
Nikola Nikolov <revolutioner11@gmail.com>
2018-11-26 02:05:19 +0200

    First Class

---
+++ Nikola-Nikolov-Employees/Nikola-Nikolov-Employees/ProjectData.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikola_Nikolov_Employees
+{
+    class ProjectData
+    {
+        public ProjectData()
+        {
+            EmployeeID = 0;
+            ProjectID = 0;
+            DateFrom = default(DateTime);
+            DateTo = default(DateTime);
+            TimeElapsed = default(TimeSpan);
+        }
+
+        public ProjectData(int EmployeeID, int ProjectID, DateTime DateFrom, DateTime DateTo)
+        {
+            this.EmployeeID = EmployeeID;
+            this.ProjectID = ProjectID;
+            this.DateFrom = DateFrom;
+            this.DateTo = DateTo;
+        }
+
+
+        private int employeeID;
+        private int projectID;
+        private DateTime dateFrom;
+        private DateTime dateTo;
+        private TimeSpan timeElapsed;
+
+        public int EmployeeID
+        {
+            get
+            {
+                return employeeID;
+            }
+
+            set
+            {
+                employeeID = value;
+            }
+        }
+        public int ProjectID
+        {
+            get
+            {
+                return projectID;
+            }
+
+            set
+            {
+                projectID = value;
+            }
+        }
+        public DateTime DateFrom
+        {
+            get
+            {
+                return dateFrom;
+            }
+
+            set
+            {
+                dateFrom = value;
+            }
+        }
+        public DateTime DateTo
+        {
+            get
+            {
+                return dateTo;
+            }
+
+            set
+            {
+                dateTo = value;
+            }
+        }
+        public TimeSpan TimeElapsed
+        {
+            get
+            {
+                return timeElapsed;
+            }
+
+            set
+            {
+                timeElapsed = value;
+            }
+        }
+    }
+}
